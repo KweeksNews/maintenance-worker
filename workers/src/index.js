df1e7e8d7ec0521674353bec07ebd6181516b339
Ahmad Rulim <mfalkia342@gmail.com>
2021-07-26 21:27:05 +0700

    Refactor code
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- workers/src/index.js
+++ workers/src/index.js
@@ -2,32 +2,15 @@ import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 
 const handleEvent = async (event) => {
   const options = {
-    cacheControl: {
-      bypassCache: true,
-    },
+    cacheControl: { bypassCache: true },
     mapRequestToAsset: (req) => new Request(`${new URL(req.url).origin}/index.html`, req),
   };
 
-  const allowedPath = [
-    'apple-touch-icon.png',
-    'favicon-16x16.png',
-    'favicon-32x32.png',
-    'favicon.ico',
-    'assets/',
-  ];
   const allowedIp = JSON.parse(await KV.get('allowed-ip'));
   const allowedHostname = JSON.parse(await KV.get('allowed-hostname'));
-  const ip = event.request.headers.get('cf-connecting-ip');
-  const { hostname, pathname } = new URL(event.request.url);
 
-  if (new RegExp(`^(${allowedPath.join('|')})`).test(pathname.substring(1))) {
-    try {
-      return await getAssetFromKV(event);
-    } catch (e) {
-      const res = await getAssetFromKV(event, options);
-      return new Response(res.body, { ...res, status: 503 });
-    }
-  }
+  const ip = event.request.headers.get('cf-connecting-ip');
+  const { hostname } = new URL(event.request.url);
 
   if (allowedIp.includes(ip)) {
     return fetch(event.request);
@@ -44,8 +27,12 @@ const handleEvent = async (event) => {
     return response;
   }
 
-  const response = await getAssetFromKV(event, options);
-  return new Response(response.body, { ...response, status: 503 });
+  try {
+    return await getAssetFromKV(event);
+  } catch (e) {
+    const res = await getAssetFromKV(event, options);
+    return new Response(res.body, { ...res, status: 503 });
+  }
 };
 
 addEventListener('fetch', (event) => {
